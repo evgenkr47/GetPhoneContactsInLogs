9756f230a4edc18d3285e649597d2915c5c98daf
evgenkr47 <evgenkr47@gmail.com>
2022-09-11 18:51:10 +0700

    Initial commit

---
+++ app/src/main/java/com/example/readcontacts/Contact.kt
@@ -0,0 +1,6 @@
+package com.example.readcontacts
+
+data class Contact(
+    val id: Int,
+    val name: String
+)
---
+++ app/src/main/java/com/example/readcontacts/MainActivity.kt
@@ -0,0 +1,83 @@
+package com.example.readcontacts
+
+import android.content.pm.PackageManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.ContactsContract
+import android.util.Log
+import androidx.core.app.ActivityCompat
+import kotlin.concurrent.thread
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        permissionGranted()
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == READ_CONTACTS_RC && grantResults.isNotEmpty()){
+            val permissionGranted = grantResults[0] == PackageManager.PERMISSION_GRANTED
+            if (permissionGranted){
+                requestContacts()
+            }
+            else {
+                Log.d("MainActivity", "Permission Denied")
+            }
+
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    private fun requestPermission(){
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(android.Manifest.permission.READ_CONTACTS),
+            READ_CONTACTS_RC
+        )
+    }
+
+    private fun requestContacts(){
+        thread {
+            val cursor = contentResolver.query(
+                ContactsContract.Contacts.CONTENT_URI,
+                null,
+                null,
+                null,
+                null
+            )
+
+            while (cursor?.moveToNext() == true){
+                val id = cursor.getInt(cursor.getColumnIndexOrThrow( ContactsContract.Contacts._ID))
+                val name = cursor.getString(cursor.getColumnIndexOrThrow( ContactsContract.Contacts.DISPLAY_NAME))
+                val contact = Contact(
+                    id, name
+                )
+                Log.d("MainActivity", contact.toString())
+            }
+            cursor?.close()
+        }
+    }
+
+    private fun permissionGranted(){
+        val permissionGranted = ActivityCompat.checkSelfPermission(
+            this,
+            android.Manifest.permission.READ_CONTACTS
+        ) == PackageManager.PERMISSION_GRANTED
+
+        if (permissionGranted){
+            requestContacts()
+        }
+        else {
+            requestPermission()
+        }
+    }
+
+    companion object{
+        private const val READ_CONTACTS_RC = 100
+    }
+}
